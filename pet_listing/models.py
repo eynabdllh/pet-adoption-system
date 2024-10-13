9f5ec8bb5e6d318e1bf8ca59ec3977dd8578c4e0
anamgndia <abdullanamerah@gmail.com>
2024-10-13 19:43:04 +0800

    pet listing for adopter dashboard

--- pet_listing/models.py
+++ pet_listing/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    pet_type = models.CharField(max_length=50)
+    breed = models.CharField(max_length=100)
+    description = models.TextField()
+    is_available = models.BooleanField(default=True)
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    ]
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True)
+    adoption_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0) 
+    main_image = models.ImageField(upload_to='pets/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class PetImage(models.Model):
+    pet = models.ForeignKey(Pet, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='pets/')
+
+    def __str__(self):
+        return f"{self.pet.name} Image"
