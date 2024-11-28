3462ea9ce165a66e56e019cb13d564f690cc610e
anamgndia <abdullanamerah@gmail.com>
2024-11-28 21:46:17 +0800

    [Ana] added urls for dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,8 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path('', views.adopter_dashboard, name='adopter_dashboard'),
     path('admin/', views.admin_dashboard, name='admin_dashboard'),
+    path('pet/<int:pet_id>/', views.view_pet_detail, name='view_pet_detail'),
+    path('review_form/<int:pet_id>/', views.review_form_detail, name='review_form_detail'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
