e778fa3aadb5bcfd6b8ab4b9f9583a6387e9740a
Chrizmas20 <chrizzaarnie.gales@cit.edu>
2024-11-30 01:04:40 +0800

    added x and check buttons

--- request_form/models.py
+++ request_form/models.py
@@ -14,7 +14,6 @@ class Adoption(models.Model):
 
     REASON_CHOICES = [
         ('duplicate_request', 'Duplicate Request'),
-        ('not_picking_up', 'Not Picking Up Pet on Time'),
         ('overwhelmed_adopter', 'Overwhelmed Adopter'),
         ('ethical_safety_concerns', 'Ethical or Safety Concerns'),
         ('inability_to_meet_needs', 'Inability to Meet Pet’s Needs'),
